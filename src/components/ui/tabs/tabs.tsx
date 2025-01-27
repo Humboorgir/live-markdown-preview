db5b12887e92c0259f0af0421d8a33e6ac1f2483
Humboorgir <hardcraftap@gmail.com>
2025-01-27 20:35:33 +0330

    fix: remove unnecessary code

--- src/components/ui/tabs/tabs.tsx
+++ src/components/ui/tabs/tabs.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React, { useContext, useState, createContext, memo, useMemo, useEffect } from "react";
 import { motion } from "framer-motion";
 import Button from "@/components/ui/button";
